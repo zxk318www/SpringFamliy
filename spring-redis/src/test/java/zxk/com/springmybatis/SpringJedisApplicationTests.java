9099e12d38f290d94d5ee64b3603b114434e424b
zhangxike <zhangxike@wondersgroup.com>
2019-06-25 13:38:58 +0800

    1-14 spring boot + redis

---
+++ spring-redis/src/test/java/zxk/com/springmybatis/SpringJedisApplicationTests.java
@@ -0,0 +1,16 @@
+package zxk.com.springmybatis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJedisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
